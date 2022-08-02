aec2aacc2f5eff9940c04ed404548cfe94c4ca9f
sharkbyte <sgquin@gmail.com>
2022-08-02 12:24:39 -0700

    start ws server, and client at same time

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,25 +1,34 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
+import ws from "ws";
 import * as trpcNext from "@trpc/server/adapters/next";
+import { NodeHTTPCreateContextFnOptions } from "@trpc/server/dist/declarations/src/adapters/node-http";
+import EventEmitter from "events";
+import { IncomingMessage } from "http";
 import { unstable_getServerSession as getServerSession } from "next-auth";
 
 import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import { prisma } from "../db/client";
+import { getSession } from "next-auth/react";
+
+const ee = new EventEmitter();
 
 export const createContext = async (
-  opts?: trpcNext.CreateNextContextOptions,
+  opts?:
+    | trpcNext.CreateNextContextOptions
+    | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>
 ) => {
   const req = opts?.req;
   const res = opts?.res;
 
-  const session =
-    req && res && (await getServerSession(req, res, nextAuthOptions));
+  const session = req && res && (await getSession({ req }));
 
   return {
     req,
     res,
     session,
     prisma,
+    ee,
   };
 };
 
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,12 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { roomRouter } from "./room";
 import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter);
+  .merge("room.", roomRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
